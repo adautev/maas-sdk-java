49ebca8410532c47f1e8571aacd4a64b9aea5399
Elviss Kuštans <n3o59hf@gmail.com>
2016-06-29 18:58:23 +0300

    Stage endpoint

--- maas-sdk/src/main/java/com/miracl/maas_sdk/MiraclConfig.java
+++ maas-sdk/src/main/java/com/miracl/maas_sdk/MiraclConfig.java
@@ -2,5 +2,5 @@ package com.miracl.maas_sdk;
 
 public class MiraclConfig
 {
-	public static final String ISSUER = "https://api.dev.miracl.net";
+	public static final String ISSUER = "https://api.stg.miracl.net";
 }
